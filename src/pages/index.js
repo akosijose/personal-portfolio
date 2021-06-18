ae1706cb286958ad141e5dd982603f4c2fa02e3b
akosijose <user.josegeraldlumbao0307@gmail.com>
2021-06-18 20:25:20 +0800

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,17 @@ import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Jose Gerald | Personal Portfolio</title>
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
